6bf49c12637bed3516af89af5001620bace1df01
javeria2108 <javeriazh@gmail.com>
2023-09-03 20:46:41 +0500

    mongoDB connection fixed

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,16 +4,16 @@ import Image from "next/image"
 import {useState, useEffect} from 'react'
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 const Nav = () => {
-    const isUserLoggedIn=true;
+    const {data:session}=useSession();
     const [providers,setProviders]=useState(null)
     const [toggleDropdown, setToggleDropdown]=useState(false);
 
     useEffect(()=>{
-        const setProviders= async()=>{
+        const setUpProviders= async()=>{
             const response= await getProviders();
             setProviders(response)
         }
-        setProviders();
+        setUpProviders();
     })
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -26,9 +26,9 @@ const Nav = () => {
         </Image>    
         <p className="logo_text">Promptopia</p>      
         </Link>
-        {/*mobile nav */}
+       
         <div className="sm:flex hidden">
-            {isUserLoggedIn? (
+            {session?.user ? (
                 <div className="flex gap-3 md:gap-5">
                     <Link href='/create-prompt'
                     className="black_btn">
@@ -40,7 +40,7 @@ const Nav = () => {
 
                     </button>
                     <Link href="/profile">
-                        <Image src="/assets/images/logo.svg"
+                        <Image src={session?.user.image}
                         width={37}
                         height={37}
                         className="rounded-full"
@@ -64,10 +64,11 @@ const Nav = () => {
              </>}
 
         </div>
+         {/*mobile nav */}
         <div className="sm:hidden flex relative">
-            {isUserLoggedIn?(
+            {session?.user ?(
                 <div className="flex">
-                     <Image src="/assets/images/logo.svg"
+                     <Image src={session?.user.image}
                         width={37}
                         height={37}
                         className="rounded-full"
--- components/Provider.jsx
+++ components/Provider.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 import { SessionProvider } from 'next-auth/react'
 const Provider = ({children,session}) => {
   return (
+    <div>
     <SessionProvider>
         {children}
     </SessionProvider>
+    </div>
   )
 }
 
